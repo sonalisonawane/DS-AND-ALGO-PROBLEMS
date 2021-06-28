5ee446370893ad3bc5f8b87f621be150a1929d22
sonali sonawane <sonawanesonali41@gmail.com>
2021-06-28 11:33:51 +0530

    first commit

---
+++ pythonStringProblems.py
@@ -0,0 +1,73 @@
+# Reverse a string
+# newstring = "Sonali"
+# print(newstring[::-1])
+
+# takes a sentence and counts the number of upperCases lowercases alphabets and digits
+# sentence = input("Enter Sentence:")
+# upper,lower,alpha,digit = 0,0,0,0
+# for x in sentence:
+#     if x.isupper():
+#         upper += 1
+#     if x.islower():
+#         lower+=1
+#     if x.isalpha():
+#         alpha+=1
+#     if x.isdigit():
+#         digit+=1
+   
+# print("There are {}:upper  {}:lower,{}:alphabetic and {}:digits in given sentence".format(upper,lower,alpha,digit ))
+
+#PALINDROME OR NOT
+
+# newstr = input("Enter a string:")
+
+# rev = newstr[::-1]
+# if newstr == rev:
+#     print("{} is a palindrome string".format(newstr))
+# else:
+#     print("{} is not a palindrome string".format(newstr))
+
+
+# a,b,c,d = 0,0,0,0
+# for x in newstr:
+#     if x == "a":
+#         a+= 1
+#     if x == "b":
+#         b+= 1
+#     if x == "c":
+#         c+= 1
+#     if x == "d":
+#         d+= 1
+
+# maximum = max(a,b,c,d)
+
+# def convertPi(str):
+#     if len(str)==0:
+#         return
+#     if str[0] == "p" and str[1] == "i":
+#         print("3.14")
+#         res = str[2:]
+#         convertPi(res)
+#     else:
+#         print(str[0])
+#         convertPi(str[1:])
+    
+# convertPi("pippxcpifdmfbdpinmnpi")
+
+
+# Remove all duplicates from the string
+
+# def remove_duplicates(str):
+
+#         if len(str)==0:
+#                 return ""
+        
+#         ch = str[0]
+#         ans = remove_duplicates(str[1:0])
+
+#         if ch==ans[0]:
+#                 return ans;
+        
+#         return ch+ans
+
+# remove_duplicates("ssoonali")
---
+++ recursionproblems.py
@@ -0,0 +1,11 @@
+# sorted or not
+
+# arr = [2,4,5,6,7,5]
+# def sorted(arr):
+#         if len(arr)==1:
+#                 return True
+#         return arr[0]<= arr[1] and sorted(arr[1:])
+
+
+# print(sorted(arr))
+
